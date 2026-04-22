268eb573a7cabcbdf69ca3944355d8ac87020aea
ScarlettCordova <scarargecorb@gmail.com>
2026-04-22 17:38:28 -0500

    Ejercicios del video

---
+++ ejercicio.js
@@ -0,0 +1,28 @@
+function listaNumeros(){
+    for(let i=0; i<=3; i++){
+        console.log(i)
+    }
+}
+
+function ejecutar(numEjercicio){
+ if (numEjercicio==1){
+    listaNumeros()
+ }else if(numEjercicio==2){
+    listaNumerosReversa();
+ }else if(numEjercicio==3){
+    listarPares();
+ }
+}
+
+function listaNumerosReversa(){
+    for(let i=3; i>0;i--){
+        console.log(i)
+    }
+}
+
+function listarPares(){
+    for(let i=0;i<10;i+=2){
+        console.log(i)
+    }
+}
+
